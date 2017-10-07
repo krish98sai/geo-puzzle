027226b8ff9c1bdf59c67104e6fb33662f12b343
Siddartha Punj <sid.punj@gmail.com>
2017-10-07 03:55:38 -0700

    Added auth token

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  require 'auth_token'
+
+  def verify_jwt_token
+    head :unauthorized if request.headers['Authorization'].nil? ||
+        !AuthToken.valid?(request.headers['Authorization'].split(' ').last)
+  end
+
 end
---
+++ backend/app/controllers/users/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Users::SessionsController < Devise::SessionsController
+  require 'auth_token'
+  #Skips CSRF protection if JSON request, protects if web request
+  skip_before_action :verify_authenticity_token, :if => lambda{ request.headers["ACCEPT"] == "application/json" }
+  respond_to :html, :json
+
+  #Adapted to support login and auth_token distribution over JSON
+  def create
+    params[:user].merge!(remember_me: 1)
+    #Checks to see if request is coming from JSON
+    if request.headers["ACCEPT"] == "application/json"
+      self.resource = warden.authenticate!(auth_options)
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+
+      # Generate auth_token and render JSON after authenticating
+      token = AuthToken.issue_token({ user: resource.id })
+      respond_with resource, location: after_sign_in_path_for(resource) do |format|
+        format.json { render json: { user: resource.email, token: token } }
+      end
+    else
+      super
+    end
+  end
+end
