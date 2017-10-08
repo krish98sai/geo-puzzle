e29c0afbbacaeccdd6d49900ca088800f92daeed
Siddartha Punj <sid.punj@gmail.com>
2017-10-08 06:20:56 -0700

    Plays video on screen depending on location of virtual geocache

---
+++ frontend/screens/ViewPostScreen.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  AppRegistry,
+  Text,
+  View,
+  Button,
+  Screen,
+  TextInput,
+  StyleSheet,
+  TouchableHighlight,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  Keyboard,
+  Image,
+  Alert
+} from 'react-native';
+
+import { StackNavigator } from 'react-navigation';
+import { Camera, Permissions, Video } from 'expo';
+class ViewPostScreen extends React.Component {
+
+  static navigationOptions = {
+    title: 'ViewPosts',
+  }
+
+  constructor(props) {
+    super(props);
+    this.onPressButton = this.onPressButton.bind(this);
+  }
+  onPressButton(){
+    this.vid.presentFullscreenPlayer();
+    this.vid.seek(5);
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+   return (
+     <View style={styles.container}>
+       <TouchableHighlight onPress={this.onPressButton}>
+       <Text style={{color: 'yellow'}}>Full Screen</Text>
+
+       </TouchableHighlight>
+       <Video
+         ref={r=>this.vid = r}
+         source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+         rate={1.0}
+         volume={1.0}
+         muted={false}
+         resizeMode="cover"
+         repeat
+         style={{ width: 300, height: 300 }}
+       />
+     </View>
+   );
+ }
+}
+
+export default myapp = StackNavigator({
+  ViewPost: { screen: ViewPostScreen},
+});
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   alignItems: 'center',
+   justifyContent: 'center',
+   backgroundColor: '#rgba(0,0,0,0.9)',
+ },
+});
