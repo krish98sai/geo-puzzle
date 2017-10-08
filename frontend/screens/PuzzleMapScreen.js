eb98dc9abefd6ed067e726012e5d4564317fd1ef
Saikrishna Rao <krish98.sai@gmail.com>
2017-10-08 02:36:58 -0700

    Testing map following location

--- frontend/screens/PuzzleMapScreen.js
+++ frontend/screens/PuzzleMapScreen.js
@@ -13,74 +13,89 @@ import {
   Image,
   Alert
 } from 'react-native';
+import {
+  MapView,
+  Permissions,
+  Location
+} from 'expo';
 
 import { StackNavigator } from 'react-navigation';
-import { Camera, Permissions } from 'expo';
 class PuzzleMapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: {
+      coords: {
+        latitude: 0.0,
+        longitude: 0.0,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }
+    }
+    }
+    this.onRegionChange = this.onRegionChange.bind(this);
+  }
+
   static navigationOptions = {
     title: 'Nearby Puzzle Map',
     header: null
   };
-  state = {
-    password: "",
-    username: ""
+
+  componentDidMount() {
+    this._getLocationAsync();
   }
+
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }
+    };
+  }
+
+  onRegionChange(location) {
+    this.setState({location});
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    Location.watchPositionAsync({
+      enableHighAccuracy: true,
+      timeInterval: 1000,
+      distanceInterval: 5
+    }, location => {
+      location.coords.latitudeDelta = 0.02;
+      location.coords.longitudeDelta = 0.02;
+      this.setState({location})
+    });
+  };
+
   render() {
     const { navigate } = this.props.navigation;
-    return (
-    /*  <View style={[styles.background]}>
-        <Text style = {{
-          color:'black'
-
-        }}>Hello, Chat App!</Text>
-        <Button
-          color="#66C8f3"
-          backgroundColor="#6688ff"
-
-          onPress={() => navigate('CreateAcct')}
-          title="CreateAccount"
-        />
-      </View>*/
-
-
-
+    return(
       <View style={styles.container}>
-        <View style={styles.logoBox}>
-          <Image
-          style={styles.logo}
-          source={require('../resource/images/Logo.png')}
-          />
+        <Text style={styles.topText}>Location of Next Clue</Text>
+
+        <MapView
+          style={styles.map}
+          region={this.state.location.coords}
+          onRegionChange={this.onRegionChange}>
+        </MapView>
+
+        <View style={styles.buttonView}>
+          <Button title="PRESS ME">PRESS ME</Button>
         </View>
-        <KeyboardAvoidingView behavior="padding"  style={styles.loginstuff}>
-          <TextInput style={styles.input}
-            placeholder= "username"
-            placeholderTextColor= "rgba(0,0,0,0.7)"
-            returnKeyType ="next"
-            underlineColorAndroid='transparent'
-            keyboardType= "email-address"
-            onSubmitEditing={() => this.passwordInput.focus()}
-            ref={(input) => {this.state.username = input}}
-          />
-
-          <TextInput style={styles.input}
-            placeholder= "password"
-            secureTextEntry
-            placeholderTextColor= "rgba(0,0,0,0.7)"
-            returnKeyType ="go"
-            underlineColorAndroid='transparent'
-            ref={(input) => {this.state.password = input}}
-
-          />
-          <TouchableOpacity style={styles.button} onPress={() => navigate("CreateAcct")}>
-            <Text>
-            LOGIN
-            </Text>
-          </TouchableOpacity>
-
-        </KeyboardAvoidingView>
-
       </View>
-
     );
   }
 }
@@ -90,55 +105,16 @@ export default myapp = StackNavigator({
 });
 
 const styles = StyleSheet.create({
-
-  /*buttoner: {
-    color:"black",
-    width: 50,
-    backgroundColor:"white",
-  },
-  background: {
-    flex:1,
-    backgroundColor:'#66b8D3',
-  },*/
-
   container: {
     flex: 1,
-    backgroundColor:'#3498db',
-
   },
-  logoBox:{
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  topText: {
+    flex: 2
   },
-  logo:{
-    width: 200,
-    height: 200,
-
+  map: {
+    flex: 9
   },
-  title: {
-    color: '#FFF',
-    marginTop: 10,
-    width: 160,
-    textAlign: 'center',
-  },
-  loginstuff: {
-    flex: 2,
-    padding: 20,
-  },
-  input: {
-    height: 40,
-    backgroundColor: '#FFF',
-    marginBottom: 20,
-    color: '#000',
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  button:{
-    alignItems: 'center',
-    backgroundColor: '#2980b9',
-    paddingVertical: 10,
+  buttonView: {
+    flex: 2
   }
-
-
 });
