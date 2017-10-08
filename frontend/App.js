4ff2f6865d6e62d7e7053089e3c8af17a8223635
Siddartha Punj <sid.punj@gmail.com>
2017-10-08 03:06:01 -0700

    Currently able to play mp4 videos inside app, but not linked with other screens yet

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font } from 'expo';
+import { Platform, StatusBar, StyleSheet, View, TouchableHighlight, Text } from 'react-native';
+import { AppLoading, Asset, Font, Constants, Components, Video } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import RootNavigation from './navigation/RootNavigation';
 
-export default class App extends React.Component {
-  state = {
+
+
+  /*state = {
     isLoadingComplete: false,
   };
 
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+    return (
+    /*if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
         <AppLoading
           startAsync={this._loadResourcesAsync}
@@ -65,5 +67,45 @@ const styles = StyleSheet.create({
   statusBarUnderlay: {
     height: 24,
     backgroundColor: 'rgba(0,0,0,0.2)',
+  },
+});*/
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onPressButton = this.onPressButton.bind(this);
+  }
+  onPressButton(){
+    this.vid.presentFullscreenPlayer();
+    this.vid.seek(5);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this.onPressButton}>
+        <Text style={{color: 'yellow'}}>Full Screen</Text>
+        </TouchableHighlight>
+        <Video
+          ref={r=>this.vid = r}
+          source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+          rate={1.0}
+          volume={1.0}
+          muted={false}
+          resizeMode="cover"
+          repeat
+          style={{ width: 300, height: 300 }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#rgba(0,0,0,0.9)',
   },
 });
